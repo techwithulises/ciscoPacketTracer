db2e6032bd931b5a6dee6becbe6061f48eddde6b
ulisesGutierrez <techwithulises@gmail.com>
2023-04-27 15:53:18 -0600

    intro

---
+++ aaa_intro.py
@@ -0,0 +1,9 @@
+"""
+Packet Tracer
+Packet Tracer es una herramienta que permite simular redes reales.
+Proporcione tres menús principales que puede utilizar para lo siguiente:
+
+Puede agregar dispositivos y conectarlos a través de cables o de forma inalámbrica.
+Puede seleccionar, eliminar, inspeccionar, etiquetar y agrupar componentes dentro de la red.
+Administre su red
+"""
